f0a8f28933586ad3f75c27091cdc13427af33a53
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-09-09 21:43:56 -0300

    navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Navbar from "./components/Navbar"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <div className='bg-red-200 h-screen w-full'>
-     Ares
-   </div>
+    <>
+      <Navbar />
+      <h1>asd</h1>
+   </>
   )
 }
 
---
+++ src/components/ContactButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@chakra-ui/react'
+
+const ContactButton = () => {
+  return (
+    
+    <div className="py-2 px-6 rounded md:ml-4">
+      <Button colorScheme='blue'>Contacto</Button>
+    </div>
+    )
+}
+
+export default ContactButton
