89c7f85c4832a2d74920474660250417c3ec68ca
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-09-21 01:17:59 -0300

    se agrega carousel a featured products

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
-import FeaturedProducts from "./components/FeaturedProducts"
+import CarouselFeaturedProducts from "./components/CarouselFeaturedProducts"
 import AboutUs from "./components/AboutUs"
 import MainProducts from "./components/MainProducts"
 
@@ -17,7 +17,11 @@ function App() {
         
         <main>    
           <Hero />
-          <FeaturedProducts />
+          <div className="text-center m-5">
+          <h2 className="font-bold tracking-tight text-indigo-600 text-3xl">Productos Destacados</h2>
+
+            <CarouselFeaturedProducts />
+          </div>
           <AboutUs />
           <MainProducts />
          <Footer />
